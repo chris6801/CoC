88a926c571bf6e9e026f33f381efa833ba96e034
chris6801 <94729095+chris6801@users.noreply.github.com>
2022-07-16 18:23:49 -0700

    Add files via upload

--- tile_types.py
+++ tile_types.py
@@ -1,33 +1,33 @@
-from os import walk
 from typing import Tuple
 
-import numpy as np # type: ignore
+import numpy as np  # type: ignore
 
-# tile graphics structured type compatible with Console.tile_rgb
+# Tile graphics structured type compatible with Console.tiles_rgb.
 graphic_dt = np.dtype(
     [
-        ("ch", np.int32), # unicode code point
-        ("fg", "3B"), # 3 unisigned bytes, for RGB colors
-        ("bg", "3B")
+        ("ch", np.int32),  # Unicode codepoint.
+        ("fg", "3B"),  # 3 unsigned bytes, for RGB colors.
+        ("bg", "3B"),
     ]
 )
 
-# tile struct used for statically defined tile data
+# Tile struct used for statically defined tile data.
 tile_dt = np.dtype(
     [
-        ("walkable", np.bool), # true if tile is walkable
-        ("transparent", np.bool), # true if not blocking FOV
-        ("dark", graphic_dt), # graphics for when tile is no in FOV
+        ("walkable", np.bool),  # True if this tile can be walked over.
+        ("transparent", np.bool),  # True if this tile doesn't block FOV.
+        ("dark", graphic_dt),  # Graphics for when this tile is not in FOV.
     ]
 )
 
+
 def new_tile(
-    *, # enforce the use of keywords, so the paramater order doesn't matter
+    *,  # Enforce the use of keywords, so that parameter order doesn't matter.
     walkable: int,
     transparent: int,
     dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
 ) -> np.ndarray:
-    """Helper function for defining individual file types"""
+    """Helper function for defining individual tile types """
     return np.array((walkable, transparent, dark), dtype=tile_dt)
 
 
@@ -35,5 +35,5 @@ floor = new_tile(
     walkable=True, transparent=True, dark=(ord(" "), (255, 255, 255), (50, 50, 150)),
 )
 wall = new_tile(
-    walkable=False, transparent=False, dark=(ord(" "), (255, 255, 255,), (0, 0, 100)),
+    walkable=False, transparent=False, dark=(ord(" "), (255, 255, 255), (0, 0, 100)),
 )
