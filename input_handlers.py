b632798c868f6d132c5de45ecba31d0c88399851
chris6801 <94729095+chris6801@users.noreply.github.com>
2022-07-25 04:08:11 -0700

    You can attack enemies with movement, no damage

--- input_handlers.py
+++ input_handlers.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 import tcod.event
 
-from actions import Action, EscapeAction, MovementAction
+from actions import Action, BumpAction, EscapeAction
 
 class EventHandler(tcod.event.EventDispatch[Action]):
     def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
@@ -14,21 +14,21 @@ class EventHandler(tcod.event.EventDispatch[Action]):
         key = event.sym
 
         if key == tcod.event.K_UP or key == tcod.event.K_KP_8:
-            action = MovementAction(dx = 0, dy = -1)
+            action = BumpAction(dx=0, dy=-1)
         elif key == tcod.event.K_DOWN or key == tcod.event.K_KP_2:
-            action = MovementAction(dx = 0, dy = 1)
+            action = BumpAction(dx=0, dy=1)
         elif key == tcod.event.K_LEFT or key == tcod.event.K_KP_4:
-            action = MovementAction(dx = -1, dy = 0)
+            action = BumpAction(dx=-1, dy=0)
         elif key == tcod.event.K_RIGHT or key == tcod.event.K_KP_6:
-            action = MovementAction(dx = 1, dy = 0)
+            action = BumpAction(dx=1, dy=0)
         elif key == tcod.event.K_KP_7:
-            action = MovementAction(dx = -1, dy = -1)
+            action = BumpAction(dx=-1, dy=-1)
         elif key == tcod.event.K_KP_9:
-            action = MovementAction(dx = 1, dy = -1)
+            action = BumpAction(dx=1, dy=-1)
         elif key == tcod.event.K_KP_1:
-            action = MovementAction(dx = -1, dy = 1)
+            action = BumpAction(dx=-1, dy=1)
         elif key == tcod.event.K_KP_3:
-            action = MovementAction(dx = 1, dy = 1)
+            action = BumpAction(dx=1, dy=1)
 
         elif key == tcod.event.K_ESCAPE:
             actionn = EscapeAction()
