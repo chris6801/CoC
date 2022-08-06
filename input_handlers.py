719f05f2adebdddeb69ae10a702babcd1b652f73
chris6801 <94729095+chris6801@users.noreply.github.com>
2022-08-06 10:21:01 -0700

    damage and death

--- input_handlers.py
+++ input_handlers.py
@@ -4,10 +4,38 @@ from typing import Optional, TYPE_CHECKING
 
 import tcod.event
 
-from actions import Action, BumpAction, EscapeAction
+from actions import Action, BumpAction, EscapeAction, WaitAction
 
 if TYPE_CHECKING:
     from engine import Engine
+
+
+MOVE_KEYS = {
+    # Arrow keys.
+    tcod.event.K_UP: (0, -1),
+    tcod.event.K_DOWN: (0, 1),
+    tcod.event.K_LEFT: (-1, 0),
+    tcod.event.K_RIGHT: (1, 0),
+    tcod.event.K_HOME: (-1, -1),
+    tcod.event.K_END: (-1, 1),
+    tcod.event.K_PAGEUP: (1, -1),
+    tcod.event.K_PAGEDOWN: (1, 1),
+    # Numpad keys.
+    tcod.event.K_KP_1: (-1, 1),
+    tcod.event.K_KP_2: (0, 1),
+    tcod.event.K_KP_3: (1, 1),
+    tcod.event.K_KP_4: (-1, 0),
+    tcod.event.K_KP_6: (1, 0),
+    tcod.event.K_KP_7: (-1, -1),
+    tcod.event.K_KP_8: (0, -1),
+    tcod.event.K_KP_9: (1, -1),
+}
+
+WAIT_KEYS = {
+    tcod.event.K_PERIOD,
+    tcod.event.K_KP_5,
+    tcod.event.K_CLEAR,
+}
     
 
 class EventHandler(tcod.event.EventDispatch[Action]):
@@ -36,22 +64,12 @@ class EventHandler(tcod.event.EventDispatch[Action]):
 
         player = self.engine.player
 
-        if key == tcod.event.K_UP or key == tcod.event.K_KP_8:
-            action = BumpAction(player, dx=0, dy=-1)
-        elif key == tcod.event.K_DOWN or key == tcod.event.K_KP_2:
-            action = BumpAction(player, dx=0, dy=1)
-        elif key == tcod.event.K_LEFT or key == tcod.event.K_KP_4:
-            action = BumpAction(player, dx=-1, dy=0)
-        elif key == tcod.event.K_RIGHT or key == tcod.event.K_KP_6:
-            action = BumpAction(player, dx=1, dy=0)
-        elif key == tcod.event.K_KP_7:
-            action = BumpAction(player, dx=-1, dy=-1)
-        elif key == tcod.event.K_KP_9:
-            action = BumpAction(player, dx=1, dy=-1)
-        elif key == tcod.event.K_KP_1:
-            action = BumpAction(player, dx=-1, dy=1)
-        elif key == tcod.event.K_KP_3:
-            action = BumpAction(player, dx=1, dy=1)
+        if key in MOVE_KEYS:
+            dx, dy = MOVE_KEYS[key]
+            action = BumpAction(player, dx, dy)
+
+        elif key in WAIT_KEYS:
+            action = WaitAction(player)
 
         elif key == tcod.event.K_ESCAPE:
             actionn = EscapeAction(player)
