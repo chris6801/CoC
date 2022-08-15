7ff0b2419022ee5ce2f09bf11894a219d363e0d7
chris6801 <94729095+chris6801@users.noreply.github.com>
2022-08-15 02:24:31 -0700

    lightning scrolls

--- entity_factories.py
+++ entity_factories.py
@@ -1,5 +1,5 @@
 from components.ai import HostileEnemy
-from components.cunsumable import HealingConsumable
+from components import cunsumable
 from components.fighter import Fighter
 from components.inventory import Inventory
 from entity import Actor, Item
@@ -34,5 +34,11 @@ health_potion = Item(
     char="!",
     color=(127, 0, 255),
     name="Health Potion",
-    consumable=HealingConsumable(amount=4),
+    consumable=cunsumable.HealingConsumable(amount=4),
+)
+lightning_scroll = Item(
+    char="~",
+    color=(255, 255, 0),
+    name="Lightning Scroll",
+    consumable=cunsumable.LightningDamageConsumable(damage=20, maximum_range=5),
 )
