08c9de7a4a809d8370775b05c76cbba5a059e82c
imagine_engine <chris6801@gmail.com>
2022-08-16 21:09:08 -0700

    confusion scrolls and targeting

--- components/cunsumable.py
+++ components/cunsumable.py
@@ -4,9 +4,10 @@ from typing import Optional, TYPE_CHECKING
 
 import actions
 import color
-import components.inventory
+import components.ai
 from components.base_component import BaseComponent
 from exceptions import Impossible
+from input_handlers import SingleRangedAttackHandler
 
 if TYPE_CHECKING:
     from entity import Actor, Item
@@ -32,6 +33,40 @@ class Consumable(BaseComponent):
         inventory = entity.parent
         if isinstance(inventory, components.inventory.Inventory):
             inventory.items.remove(entity)
+
+class ConfusionConsumable(Consumable):
+    def __init__(self, number_of_turns: int):
+        self.number_of_turns = number_of_turns
+
+    def get_action(self, consumer: Actor) -> Optional[actions.Action]:
+        self.engine.message_log.add_message(
+            "Select a target location.", color.needs_target
+        )
+        self.engine.event_handler = SingleRangedAttackHandler(
+            self.engine,
+            callback=lambda xy: actions.ItemAction(consumer, self.parent, xy),
+        )
+        return None
+
+    def activate(self, action: actions.ItemAction) -> None:
+        consumer = action.entity
+        target = action.target_actor
+
+        if not self.engine.game_map.visible[action.target_xy]:
+            raise Impossible("You cannot target an area that you cannot see.")
+        if not target:
+            raise Impossible("You must select an enemy to target.")
+        if target is consumer:
+            raise Impossible("You cannot confuse yourself!")
+
+        self.engine.message_log.add_message(
+            f"The eyes of the {target.name} look vacant, as it starts to stumble around!",
+            color.status_effect_applied,
+        )
+        target.ai = components.ai.ConfusedEnemy(
+            entity=target, previous_ai=target.ai, turns_remaining=self.number_of_turns,
+        )
+        self.consume()
 
 class HealingConsumable(Consumable):
     def __init__(self, amount: int):
--- entity_factories.py
+++ entity_factories.py
@@ -29,7 +29,12 @@ troll = Actor(
     fighter=Fighter(hp=16, defense=1, power=4),
     inventory=Inventory(capacity=0),
 )
-
+confusion_scroll = Item(
+    char="~",
+    color=(207, 63, 255),
+    name="Confusion Scroll",
+    consumable=cunsumable.ConfusionConsumable(number_of_turns=10),
+)
 health_potion = Item(
     char="!",
     color=(127, 0, 255),
